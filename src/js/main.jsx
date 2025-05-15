8fbcc66df6c6c895eb414635ab29e2016531d239
Clay Beach <wcbeach30@gmail.com>
2025-05-15 00:20:04 +0000

    Took forever but I have a sticky nav bar, now on to the rest!

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,11 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import NavBar from './components/NavBar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <NavBar/>
     <Home/>
   </React.StrictMode>,
 )
